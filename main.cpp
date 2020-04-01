423fa2d6581c49d291127efd65b1dc26628f1467
Mrr-R1 <62749497+Mrr-R1@users.noreply.github.com>
2020-04-01 10:21:09 +0200

    Début menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+        int touche[5];
+
+        for (int i=0; i<5; i++){
+            touche[i]=i;
+        }
+
+        int selection = 0;
+
+    while (1) {
+        for (int i=0; i<5; i++){
+            cout << "choix " << i << " [";
+            if (selection == i ){
+                cout << "X]" << endl;;
+            }else{
+                cout << " ]" << endl;;
+            }
+        }
+
+        if(GetAsyncKeyState(VK_UP)){
+
+        }else if(GetAsyncKeyState(VK_DOWN)){
+
+        }
+    system("cls");
+
+    }
+    cout << "Hello world" << endl;
     return 0;
 }
