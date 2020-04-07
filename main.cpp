33a78ec8c92df31aa025ee4560af6482aadb80ad
lightin <breval2000@live.fr>
2020-04-07 09:00:52 +0200

    RENOUVEAU

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
 #include <iostream>
 #include <windows.h>
-#include "Menu.h"
 #include <conio.h>
-#include "Game.h"
+
 
 
 using namespace std;
 
 
-    Game game= Game();
-           game._ready();
-
 int main(){
-    Menu* menu = new Menu();
-    int selection;
-    int debut_game = 0;
-    while(selection != 3){
-        system("cls");
-        selection = menu->menuPricipal();
-    //Nouvelle partie == 1    |   Charger partie == 0
-        Game game = Game();
-        if (selection == 0){
-            while (selection == 0){
-                game._update_delta();
-                if(GetAsyncKeyState(VK_ESCAPE)){
-                    selection = menu->menuPause();
-                }
-            }
-        }else if (selection == 1){
-            cout << "chaussure" << endl;
-        }
-    }
+
     return 0;
 }
 
