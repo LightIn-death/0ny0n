dee37eac8be318ee2de921feaba17e25cd16fdf2
Mrr-R1 <62749497+Mrr-R1@users.noreply.github.com>
2020-04-15 20:44:33 +0200

    Fix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
-
 #include <windows.h>
-#include <MMsystem.h>
 #include "Menu.h"
+#include <mmsystem.h>
 #include <iostream>
 #include <conio.h>
 #include "Room.h"
@@ -9,36 +8,18 @@ using namespace std;
 #include <ctime>
 
 
-
-
-int main(){
-    srand (time(NULL));
-    PlaySound(TEXT("song.wav"),NULL,SND_ASYNC | SND_LOOP);
-
 void base(Player* joueur)
 {
-
     bool running = true;
-
-
     while(running)
     {
         system("cls");
         int i = 1;
-
           cout << "vous avez actuellement " << joueur->Or << " pieces d'or ! \n\n\n";
-
-
         if(joueur->Or >= 50)
-
-        etage_old = etage;
-        Room salle = Room(etage,&joueur);
-        while(etage == etage_old)
         {
-
             cout << i <<" : Vous pouvez aumenter votre vie de 50 PV \n";
             i++;
-
         }
         else
         {
@@ -46,11 +27,6 @@ void base(Player* joueur)
         }
 
         cout << i << " : Retourné combattre dans la tours.\n\n\n>";
-
-
-
-
-
         int choix = 0;
         cin >> choix;
 
@@ -73,39 +49,10 @@ void base(Player* joueur)
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int main()
 {
     srand (time(NULL));
-
+    PlaySound(TEXT("song.wav"),NULL,SND_ASYNC | SND_LOOP);
     Menu* menu = new Menu();
     int selection;
     int debut_game = 0;
@@ -132,21 +79,15 @@ int main()
             }
             if(etage==0)
             {
-
                 //base
-
                 base(&joueur);
                 //cout << "la base na pas encore ete implementer\n\n";
                 joueur.soigner();
                 etage++;
-
             }
         }
 
         cout << "vous avez fini le jeux" << endl;
-
-
-
 
         system("pause");
     }
@@ -155,22 +96,4 @@ int main()
         system("exit");
     }
     return 0;
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
